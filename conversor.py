1ef48c0c33a3de8c4a4edd4c9f0b4ea65d907f57
Carlos Alba <hola@carlosalba.net>
2020-07-01 13:38:36 +0300

    conversor

---
+++ conversor.py
@@ -0,0 +1,7 @@
+dinares = input('Ingresa la cantidad de dinares a convertir: ')
+dinares = float(dinares)
+valor_dolar = 1.41
+dolares = dinares * valor_dolar
+dolares = round(dolares, 2)
+dolares = str(dolares)
+print('Tienes $' + dolares + ' dólares')
